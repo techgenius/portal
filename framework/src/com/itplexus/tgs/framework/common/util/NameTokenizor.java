351b4aaef56674ed48906441ed628a51264cd68f
mahesh <mahesh.chweetu@gmail.com>
2014-12-06 18:44:35 +0530

    This is the commit for Name Tokenizor

---
+++ framework/src/com/itplexus/tgs/framework/common/util/NameTokenizor.java
@@ -0,0 +1,17 @@
+package com.itplexus.tgs.framework.common.util;
+
+import java.util.Scanner;
+
+public class NameTokenizor {
+
+	public String[] getNamesInTokens(String name) {
+		String[] result = name.split(" ");
+		for (String data : result)
+			;//System.out.println(data);
+		
+	//	System.out.println("Data :"+result);
+		return result;
+	}
+
+
+}
---
+++ framework/src/com/itplexus/tgs/framework/test/TestClass.java
@@ -0,0 +1,27 @@
+package com.itplexus.tgs.framework.test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.itplexus.tgs.framework.common.util.NameTokenizor;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		NameTokenizor nameTkens = new NameTokenizor();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter sentence:");
+		StringBuffer buffer = new StringBuffer();
+		sc = new Scanner(sc.nextLine());
+
+		while (sc.hasNext()) {
+			buffer.append(sc.next()+" ");
+		}
+
+		String[] result = nameTkens.getNamesInTokens(buffer.toString());
+   
+		 //System.out.println(buffer.toString());
+		System.out.println( Arrays.asList(result) );
+	}
+}
