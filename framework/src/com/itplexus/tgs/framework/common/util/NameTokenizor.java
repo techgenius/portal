a626edf41c0e2670054edd10c489c3050158eb99
getmahi <getmahi2002@gmail.com>
2014-12-07 11:38:44 +0530

    Update NameTokenizor.java

--- framework/src/com/itplexus/tgs/framework/common/util/NameTokenizor.java
+++ framework/src/com/itplexus/tgs/framework/common/util/NameTokenizor.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class NameTokenizor {
 
-	public String[] getNamesInTokens(String name) {
+	public String[] getNamesInTokens(String name) throws Exception {
+		if ( name == null)
+		   throw new Exception("Invalid or Null Parameter supplied);
 		String[] result = name.split(" ");
 	//	for (String data : result)
 			;//System.out.println(data);
